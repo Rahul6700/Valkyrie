58997a395ae8efa41bb9d5a1ddee870a35756929
Rahul6700 <rahulsk4087@gmail.com>
2025-10-12 21:03:21 +0530

    feat: defining func's for COnnection

---
+++ src/Connection.cpp
@@ -0,0 +1,70 @@
+#include "Connection.hpp"
+#include <sys/socket.h>
+#include <string>
+
+// not importing 'namespace std' since we'll have a naming conflict, we have a user defined 'close' func and a 'close' system call
+
+//defining all the Connection class functions
+
+//the constructor
+Connection::Connection(const std::string& h, int p)
+{
+  this->host = h;
+  this->port = p;
+  sockfd = -1;
+}
+
+Connection::connect
+{
+  //creating socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0); // AF_INET -> IPV4 and SOCK_STREAM -> TCP
+  if(sockfd < 0)
+  {
+    std::cout << "error creating socket in Connection.connect" << endl;
+    return false;
+  }
+
+  // we need to specify server host and port dets
+  // we'll store all these details in a struct called sockaddr_in
+  struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET; // TCP protocol
+    serverAddress.sin_port = htons(port); // port number
+
+  //we currently have out host as a string, like "127.0.01". we need to convert it to binary cuz the system call accepts a binary input
+  // we use the inet_pton function to convert IPV4/6 addresses from string to binary -> return 1 on success, 0 if not a valid IP addr str, and -ve if some other error
+  if (inet_pton(AF_INET, host.c_str(), &serverAddress.sin_addr) <= 0) {
+    std::cout << "invalid address" << endl;
+    ::close(sockfd);
+    return false;
+  }
+
+  // now we connect to the server using the 'connect' system call (not the connect function we made)
+  // to tell the compiler to use the global system call and not our conenct function, we say ::connect()
+  // we give parameters as the socket fd, a ptr poiting to our host and port dets structure and the size of the struct obj
+  if (::connect(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+    std::cerr << "Connection failed" << std::endl;
+    ::close(sockfd);
+    return false;
+  }
+
+  std::cout << "connection successful to server" << std::endl;
+  return true;
+}
+
+//function closing the connection
+Connection::close()
+{
+  //check if socket is open first
+  if(sockfd >= 0)
+  {
+    ::close(sockfd); //this is global system call, not out func
+    sockfd = -1;
+    std::cout << "connection closed" << std::endl;
+  }
+}
+
+//destructor
+Connection::~Connection()
+{
+
+}
